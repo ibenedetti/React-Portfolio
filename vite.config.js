dbe865d106f51a436de4eb74c75d7f4378524ade
ibenedetti <104228179+ibenedetti@users.noreply.github.com>
2024-07-12 18:34:40 +0200

    Attempt 5

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  server: {
+    base: 'https://ibenedetti.github.io',
+  },
   assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.bin', '**/*.png', '**/*.jpg'],
-  base: "https://ibenedetti.github.io"
 });
-
